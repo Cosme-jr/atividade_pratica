a246bd1fb467793e2a15d60a6cda3da189ba6dcb
Cosme_Jr <cosmejunior1981@gmail.com>
2025-08-19 10:33:52 -0300

    alteração

--- atividade_04/questao.py
+++ atividade_04/questao.py
@@ -12,6 +12,6 @@ preco_unitario = 12.40
 quantidade = 3
 preco_total = preco_unitario * quantidade
 print(f"Produto: {nome_produto}")
-print(f"Preço unitário: R$ {preco_unitario}")
-print(f"Quantidade: {quantidade}")
-print(f"Preço total: R$ {preco_total}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade:.2f}")
+print(f"Preço total: R$ {preco_total:.2f}")
